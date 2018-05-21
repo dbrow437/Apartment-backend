ccf19bae380749187e71c07069a07f0769ccc018
learn <learn@learn10.local>
2018-05-21 14:28:28 -0700

    more code to upload

--- app/controllers/apartment_lists_controller.rb
+++ app/controllers/apartment_lists_controller.rb
@@ -1,2 +1,20 @@
 class ApartmentListsController < ApplicationController
+    def index
+      apartmentList = ApartmentList.all
+      render json: apartmentList
+   end
+
+   def create
+  # Create a new apartment
+      apartment= apartment.create(apartment_params)
+
+  # respond with our new cat
+  render json: apartment
+end
+
+# Handle strong parameters, so we are secure
+def apartment_params
+  params.require(:apartment).permit(:streetName, :streetNum, :city, :zip, :state, :manager, :managerNum, :managerHours)
+end
+
 end
