c867b3d94db4314b6ced56bb964a0e2ae12d39ce
learn <learn@learn10.localdomain>
2018-05-24 12:54:28 -0700

    updated with logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
  root "grades#index"
-  resources :grades
-  resources :users
+  resources :grades, defaults: {format: :json} # <- Update this
+  resources :users, defaults: {format: :json} # <- Update this
   post 'user_token' => 'user_token#create'
   resources :apartment_lists
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
